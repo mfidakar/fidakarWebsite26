1df7d56c0dc867f7b5b1c07237ec10b9a303f366
Mohammad Fidakar <m.fidakar2020@gmail.com>
2026-05-20 16:05:31 -0400

    Add mobile responsive layout

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks  = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
+navLinks.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => navLinks.classList.remove('open'));
+});
